684dcf7d290749427e2200a8822aed6b577f5034
Alex Traher <alex.traher@gmail.com>
2023-01-16 09:29:14 +0000

    add README

---
+++ README.md
@@ -0,0 +1,21 @@
+# React Starter
+A minimalist starter to help learn react.
+
+See the blog post this is supported by [here](<LINK>)
+
+If you're new to development and haven't completed the `web-native-stater`, I'm strongly recommend you do that - then come back here!
+
+## Getting started
+
+1. Clone this repo
+2. run `npm install` to install dependencies
+3. run `npm start` to kick off the webpack dev server
+4. visit the running app on `localhost:8080` in your browser
+5. try making some changes and build out your own application! (some classic examples are a todo app, or a receipe search app)
+
+## Where do I actually make changes though?
+To help the learning process, there's 3 emoji's to look out for across these example apps:
+
+🪵 - shown where we change the _structure_ of our application "I need a new button"
+🎨 - shown where we change _styles_ of our application "I need my button to be blue"
+🤖 - shown where we change the _functionality_ of our application "I need my button to increment a counter"
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 const App = () => {
+  /* 🤖🤖🤖 - new logic goes here!
+    You can read more about that here: https://reactjs.org/docs/handling-events.html
+    This logic can live in this spot in any component you write! 
+    You can also extract it to functions in a separate file if you prefer, then import them with `import myFunc from "./myFunc"`
+    Read more about imports here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+    You can even create custom hooks - which you can learn about here: https://beta.reactjs.org/learn/reusing-logic-with-custom-hooks
+  */
   return (
-    <h1 className='header'>hello world<span className="cursor-blink"></span></h1>
+    <>
+      <h1 className='header'>hello world<span className="cursor-blink"></span></h1>
+      {/* 🪵🪵🪵 - new structure goes here!
+          (Here you are writing JSX - it is similar, but not exactly the same as HTML)
+          (You can also create new components to help split up bits of your app - https://reactjs.org/docs/components-and-props.html#composing-components) 
+      */}
+    </>
   )
 }
 
